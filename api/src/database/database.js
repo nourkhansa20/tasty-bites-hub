e6dc0052b86a25c4951bc146288fafe69469dbc5
Nour Khansa <118853035+nourkhansa20@users.noreply.github.com>
2024-05-01 18:05:37 +0300

    update database

--- api/src/database/database.js
+++ api/src/database/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose
-    .connect('mongodb://localhost:27017/recipe-sharing-platform')
+    .connect('mongodb+srv://nourkhansa7:1RXm9gw4gBOhiPgT@tasty_bits_hub.kurwnz6.mongodb.net/tasty_bits_hub?retryWrites=true&w=majority&appName=protopia')
     .then(() => console.log("Connected to DB"))
     .catch((err) => console.log(err))
